1fe37f9103ecc7818e6f0784f77a57a4cd93d0a8
Gul Ozdemir <szan.gule3@gmail.com>
2023-09-28 09:40:24 +0100

    Full Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Elastic Beanstalk project!'));
+app.get('/', (req, res) => res.send('An AWS project to understand Elastic Beanstalk, and CI/CD on cloud!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
